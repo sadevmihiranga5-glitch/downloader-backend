7d72beff495dc4cd1f3e833ecf834b8615bde24e
sadevmihiranga5-glitch <sadevmihiranga5@gmail.com>
2026-09-26 01:48:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,5 +30,4 @@ app.post('/api/download', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
